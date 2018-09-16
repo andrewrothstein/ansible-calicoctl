576374debb11abf13fc45993ad5939df4f946e7e
Andrew Rothstein <andrew.rothstein@gmail.com>
2018-09-16 15:59:58 -0400

    up to v3.2.1. add alpine_3.8, fedora_28, ubuntu_bionic CI builds

---
+++ dl-checksum.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+VER=v3.2.1
+DIR=~/Downloads
+MIRROR=https://github.com/projectcalico/calicoctl/releases/download/$VER
+
+for PLATFORM in darwin-amd64 linux-amd64 linux-arm64 linux-ppc64le windows-amd64.exe
+do
+    wget -O $DIR/calicoctl-$PLATFORM-$VER $MIRROR/calicoctl-$PLATFORM
+done
+sha256sum $DIR/calicoctl-*-$VER
+
