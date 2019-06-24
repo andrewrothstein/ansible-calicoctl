233b36b9420aa3c8c84759961449217b6ede67ee
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-06-24 13:04:32 -0500

    up to v3.6.3

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env sh
-VER=v3.2.6
+VER=v3.6.3
 DIR=~/Downloads
 MIRROR=https://github.com/projectcalico/calicoctl/releases/download/$VER
 
-for PLATFORM in darwin-amd64 linux-amd64 linux-arm64 linux-ppc64le windows-amd64.exe
-do
-    wget -O $DIR/calicoctl-$PLATFORM-$VER $MIRROR/calicoctl-$PLATFORM
-done
-sha256sum $DIR/calicoctl-*-$VER
+dl()
+{
+    OS=$1
+    PLATFORM=$2
+    SUFFIX=${3:-}
+    URL=$MIRROR/calicoctl-$OS-$PLATFORM$SUFFIX
+    LFILE=$DIR/calicoctl-$OS-$PLATFORM-$VER$SUFFIX
+
+    if [ ! -e $LFILE ];
+    then
+        wget -q -O $LFILE $URL
+    fi
+
+    printf "  # %s\n" $URL
+    printf "  %s-%s: sha256:%s\n" $OS $PLATFORM `sha256sum $LFILE | awk '{print $1}'`
+}
+
+printf "%s:\n" $VER
+dl darwin amd64
+dl linux amd64
+dl linux arm64
+dl linux ppc64le
+dl windows amd64 .exe
+
 
